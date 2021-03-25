6d077c7acd10ecc905d5b3c15de9211cef97ce7a
Paul Whiting <paul.whiting@stage2sec.com>
2021-03-25 09:53:36 -0600

    Fix voodoo filebeat installer.

--- c2servers/install-c2server-voodoo.sh
+++ c2servers/install-c2server-voodoo.sh
@@ -9,6 +9,7 @@
 LOGFILE="redelk-install.log"
 INSTALLER="RedELK Voodoo installer"
 ELKVERSION="7.9.2"
+ELASTICVERSION="7.x"
 
 
 echoerror() {
@@ -33,7 +34,7 @@ preinstallcheck() {
             exit 1
         else
             echo "[!] Filebeat: required version is installed ($INSTALLEDVERSION). Should be good. Stopping service now before continuing installation."
-            service filebeat stop
+            sudo service filebeat stop
             ERROR=$?
             if [ $ERROR -ne 0 ]; then
                 echoerror "[X] Could not stop filebeat (Error Code: $ERROR)."
@@ -59,11 +60,11 @@ echo ""
 echo ""
 
 #if [[ $EUID -ne 0 ]]; then
-#  echo "[X] Not running as root. Exiting"
-#  exit 1
+  #echo "[X] Not running as root. Exiting"
+  #exit 1
 #fi
 
-if ! [ $# -eq 3 ] ; then
+if ! [ $# -eq 4 ] ; then
     echo "[X] ERROR Incorrect amount of parameters"
     echo "[X] require 1st parameter: identifier of this machine to set in filebeat config."
     echo "[X] require 2nd parameter: attackscenario name."
@@ -78,8 +79,8 @@ preinstallcheck
 #set locale for current session and default locale
 echo "[*] Setting locale" | tee -a $LOGFILE
 export LC_ALL="en_US.UTF-8"
-printf 'LANG=en_US.UTF-8\nLC_ALL=en_US.UTF-8\n' > /etc/default/locale >> $LOGFILE 2>&1
-locale-gen >> $LOGFILE 2>&1
+printf 'LANG=en_US.UTF-8\nLC_ALL=en_US.UTF-8\n' > sudo tee /etc/default/locale >> $LOGFILE 2>&1
+sudo locale-gen >> $LOGFILE 2>&1
 
 
 echo "[*] Adding GPG key of Elastic" | tee -a $LOGFILE
@@ -90,15 +91,15 @@ if [ $ERROR -ne 0 ]; then
 fi
 
 echo "[*] Installing apt-transport-https" | tee -a $LOGFILE
-apt-get install -y apt-transport-https >> $LOGFILE 2>&1
+sudo apt-get install -y apt-transport-https >> $LOGFILE 2>&1
 ERROR=$?
 if [ $ERROR -ne 0 ]; then
     echoerror "[X] Could not install apt-transport-https (Error Code: $ERROR)."
 fi
 
 echo "[*] Adding Elastic APT repository" | tee -a $LOGFILE
-if [ ! -f  /etc/apt/sources.list.d/elastic-6.x.list ]; then
-    echo "deb https://artifacts.elastic.co/packages/6.x/apt stable main" | tee -a /etc/apt/sources.list.d/elastic-6.x.list >> $LOGFILE 2>&1
+if [ ! -f  /etc/apt/sources.list.d/elastic-$ELASTICVERSION.list ]; then
+    echo "deb https://artifacts.elastic.co/packages/$ELASTICVERSION/apt stable main" | sudo tee -a /etc/apt/sources.list.d/elastic-$ELASTICVERSION.list >> $LOGFILE 2>&1
 fi
 ERROR=$?
 if [ $ERROR -ne 0 ]; then
@@ -106,91 +107,92 @@ if [ $ERROR -ne 0 ]; then
 fi
 
 echo "[*] Updating APT" | tee -a $LOGFILE
-apt-get update  >> $LOGFILE 2>&1
+sudo apt-get update  >> $LOGFILE 2>&1
 ERROR=$?
 if [ $ERROR -ne 0 ]; then
     echoerror "[X] Could not update APT (Error Code: $ERROR)."
 fi
 
 echo "[*] Installing filebeat" | tee -a $LOGFILE
-apt-get install -y filebeat=$ELKVERSION >> $LOGFILE 2>&1
+sudo apt-get install -y filebeat=$ELKVERSION >> $LOGFILE 2>&1
 ERROR=$?
 if [ $ERROR -ne 0 ]; then
     echoerror "[X] Could not install filebeat (Error Code: $ERROR)."
 fi
 
 echo "[*] Setting filebeat to auto start after reboot" | tee -a $LOGFILE
-systemctl enable filebeat >> $LOGFILE 2>&1
+sudo systemctl enable filebeat >> $LOGFILE 2>&1
 ERROR=$?
 if [ $ERROR -ne 0 ]; then
     echoerror "[X] Could not change auto boot settings (Error Code: $ERROR)."
 fi
 
 echo "[*] Making backup of original filebeat config" | tee -a $LOGFILE
-mv /etc/filebeat/filebeat.yml /etc/filebeat/filebeat.yml.ori >> $LOGFILE 2>&1
+sudo mv /etc/filebeat/filebeat.yml /etc/filebeat/filebeat.yml.ori >> $LOGFILE 2>&1
 ERROR=$?
 if [ $ERROR -ne 0 ]; then
     echoerror "[X] Could not make backup (Error Code: $ERROR)."
 fi
 
 echo "[*] Copying new config file" | tee -a $LOGFILE
-cp ./filebeat/filebeat_voodoo.yml /etc/filebeat/filebeat.yml >> $LOGFILE 2>&1
+sudo cp ./filebeat/filebeat_voodoo.yml /etc/filebeat/filebeat.yml >> $LOGFILE 2>&1
 ERROR=$?
 if [ $ERROR -ne 0 ]; then
     echoerror "[X] Could not copy filebeat config (Error Code: $ERROR)."
 fi
 
 echo "[*] Copying ca file" | tee -a $LOGFILE
-cp ./filebeat/redelkCA.crt /etc/filebeat/ >> $LOGFILE 2>&1
+sudo cp ./filebeat/redelkCA.crt /etc/filebeat/ >> $LOGFILE 2>&1
 ERROR=$?
 if [ $ERROR -ne 0 ]; then
     echoerror "[X] Could not copy ca file (Error Code: $ERROR)."
 fi
 
 echo "[*] Altering hostname field in filebeat config" | tee -a $LOGFILE
-sed -i s/'@@HOSTNAME@@'/$1/g /etc/filebeat/filebeat.yml  >> $LOGFILE 2>&1
+sudo sed -i s/'@@HOSTNAME@@'/$1/g /etc/filebeat/filebeat.yml  >> $LOGFILE 2>&1
 ERROR=$?
 if [ $ERROR -ne 0 ]; then
     echoerror "[X] Could not change hostname field in filebeat config (Error Code: $ERROR)."
 fi
 
 echo "[*] Altering attackscenario field in filebeat config" | tee -a $LOGFILE
-sed -i s/'@@ATTACKSCENARIO@@'/$2/g /etc/filebeat/filebeat.yml >> $LOGFILE 2>&1
+sudo sed -i s/'@@ATTACKSCENARIO@@'/$2/g /etc/filebeat/filebeat.yml >> $LOGFILE 2>&1
 ERROR=$?
 if [ $ERROR -ne 0 ]; then
     echoerror "[X] Could not change attackscenario field in filebeat config (Error Code: $ERROR)."
 fi
 
 echo "[*] Altering log destination field in filebeat config" | tee -a $LOGFILE
-sed -i s/'@@HOSTANDPORT@@'/$3/g /etc/filebeat/filebeat.yml >> $LOGFILE 2>&1
+sudo sed -i s/'@@HOSTANDPORT@@'/$3/g /etc/filebeat/filebeat.yml >> $LOGFILE 2>&1
 ERROR=$?
 if [ $ERROR -ne 0 ]; then
     echoerror "[X] Could not change log destination field in filebeat config (Error Code: $ERROR)."
 fi
 
 echo "[*] Altering log source field in filebeat config" | tee -a $LOGFILE
-sed -i s/'@@VOODOO_LP_DIR@@'/$4/g /etc/filebeat/filebeat.yml >> $LOGFILE 2>&1
+lp_dir=$(echo "$4" | sed 's/\//\\\//g')
+sudo sed -i s/'@@VOODOO_LP_DIR@@'/$lp_dir/g /etc/filebeat/filebeat.yml >> $LOGFILE 2>&1
 ERROR=$?
 if [ $ERROR -ne 0 ]; then
     echoerror "[X] Could not change log destination field in filebeat config (Error Code: $ERROR)."
 fi
 
 echo "[*] Starting filebeat" | tee -a $LOGFILE
-service filebeat start >> $LOGFILE 2>&1
+sudo service filebeat start >> $LOGFILE 2>&1
 ERROR=$?
 if [ $ERROR -ne 0 ]; then
     echoerror "[X] Could not start filebeat (Error Code: $ERROR)."
 fi
 
 echo "[*] Creating RedELK log directory" | tee -a $LOGFILE
-mkdir -p /var/log/redelk >> $LOGFILE 2>&1
+sudo mkdir -p /var/log/redelk >> $LOGFILE 2>&1
 ERROR=$?
 if [ $ERROR -ne 0 ]; then
     echoerror "[X] Could not create RedELK log directory (Error Code: $ERROR)."
 fi
 
 echo "[*] Restarting filebeat" | tee -a $LOGFILE
-service filebeat restart >> $LOGFILE 2>&1
+sudo service filebeat restart >> $LOGFILE 2>&1
 ERROR=$?
 if [ $ERROR -ne 0 ]; then
     echoerror "[X] Could not restart filebeat (Error Code: $ERROR)."
